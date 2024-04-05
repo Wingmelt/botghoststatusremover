d6dcd852f037f4cb424df2d6a137d67f42b08d03
Wingmelt <114381226+Wingmelt@users.noreply.github.com>
2024-04-05 20:20:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Watching Simple Advertising"];
 
 
 let currentIndex = 0;
